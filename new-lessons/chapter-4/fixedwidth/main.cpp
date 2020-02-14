3d9420935474d809c0a3a18985d543e93462dda0
yafiimo <mohammedyafii@upgradepack.com>
2020-02-14 17:32:02 +0000

    floating point nums

--- new-lessons/chapter-4/fixedwidth/main.cpp
+++ new-lessons/chapter-4/fixedwidth/main.cpp
@@ -4,8 +4,18 @@
 int main()
 {
 
-    std::int_least16_t x(65538);
+    std::cout << "Fast 8:  " << sizeof(std::int_fast8_t) * 8 << " bits." << '\n';
+    std::cout << "Fast 16: " << sizeof(std::int_fast16_t) * 8 << " bits." << '\n';
+    std::cout << "Fast 32: " << sizeof(std::int_fast32_t) * 8 << " bits." << '\n';
+    std::cout << "Fast 64: " << sizeof(std::int_fast64_t) * 8 << " bits." << '\n';
 
-    std::cout << "My int is " << x << '\n';
+    std::cout << "Least 8:  " << sizeof(std::int_least8_t) * 8 << " bits." << '\n';
+    std::cout << "Least 16: " << sizeof(std::int_least16_t) * 8 << " bits." << '\n';
+    std::cout << "Least 32: " << sizeof(std::int_least32_t) * 8 << " bits." << '\n';
+    std::cout << "Least 64: " << sizeof(std::int_least64_t) * 8 << " bits." << '\n';
+
+    size_t x = sizeof(size_t);
+    std::cout << "size_t of size_t = " << x << '\n';
+    
     return 0;
 }
