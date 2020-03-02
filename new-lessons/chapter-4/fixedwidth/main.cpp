130b4f7f08783b491d7e89c62054ce7bada00d9d
yafiimo <mohammed.yafii@hotmail.co.uk>
2020-03-02 07:30:21 +0000

    revise

--- new-lessons/chapter-4/fixedwidth/main.cpp
+++ new-lessons/chapter-4/fixedwidth/main.cpp
@@ -3,6 +3,16 @@
 
 int main()
 {
+/* ---------- The following fixed width ints are not guaranteed to be defined ---------- */
+    std::cout << "Fixed 8:  " << sizeof(std::int8_t) * 8 << " bits." << '\n';
+    std::cout << "Fixed u8:  " << sizeof(std::uint8_t) * 8 << " bits." << '\n';
+    std::cout << "Fixed 16:  " << sizeof(std::int16_t) * 8 << " bits." << '\n';
+    std::cout << "Fixed u16:  " << sizeof(std::uint16_t) * 8 << " bits." << '\n';
+    std::cout << "Fixed 32:  " << sizeof(std::int32_t) * 8 << " bits." << '\n';
+    std::cout << "Fixed u32:  " << sizeof(std::uint32_t) * 8 << " bits." << '\n';
+    std::cout << "Fixed 64:  " << sizeof(std::int64_t) * 8 << " bits." << '\n';
+    std::cout << "Fixed u64:  " << sizeof(std::uint64_t) * 8 << " bits." << '\n';
+/* ------------------------------------------------------------------------------------- */
 
     std::cout << "Fast 8:  " << sizeof(std::int_fast8_t) * 8 << " bits." << '\n';
     std::cout << "Fast 16: " << sizeof(std::int_fast16_t) * 8 << " bits." << '\n';
@@ -19,3 +29,11 @@ int main()
     
     return 0;
 }
+
+/*
+    * Favor the std::int_fast#_t and std::int_least#_t integers when you need an 
+      integer guaranteed to be at least a certain minimum size.
+
+    * Avoid the 8-bit fixed-width integer types. If you do use them, note that 
+      they are often treated like chars.
+*/
